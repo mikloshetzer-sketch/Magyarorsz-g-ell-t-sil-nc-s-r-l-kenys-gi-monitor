11d420a487edcbef4f5ff550fa977f98d90859f3
mikloshetzer-sketch <mikloshetzer@gmail.com>
2026-04-07 18:53:06 +0200

    Create hungary-counties.js

---
+++ hungary-counties.js
@@ -0,0 +1,49 @@
+const countiesData = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Győr-Moson-Sopron",
+        "risk": 75
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [17.6504, 47.6875]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Pest",
+        "risk": 70
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [19.0402, 47.4979]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Hajdú-Bihar",
+        "risk": 65
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [21.6273, 47.5316]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Bács-Kiskun",
+        "risk": 60
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [19.6897, 46.8964]
+      }
+    }
+  ]
+};
